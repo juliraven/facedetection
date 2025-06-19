2644a61cab6a3a25ba817ce7fd828d62446d5222
julia <168024513+juliraven@users.noreply.github.com>
2025-06-19 16:06:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -82,11 +82,10 @@ with tabs[0]:
     st.markdown('')
 
     styled_container = st.container()
-        st.markdown("<div id='outer_marker'></div>", unsafe_allow_html=True)
-
-        with styled_container:
-            st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
-            st.write('Celem naszego projektu było zbudowanie sieci neuronowej, która potrafi wykrywać ludzkie twarze na obrazach, a także w czasie rzeczywistym, np. z kamery w laptopie. Zbudowałyśmy także model rozpoznający (klasyfikujący) konkretne twarze, który wykorzystuje wiedzę na temat wykrywania dowolnych twarzy i jest rozszerzeniem zagadnienia detekcji twarzy.')
+    st.markdown("<div id='outer_marker'></div>", unsafe_allow_html=True)
+    with styled_container:
+        st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
+        st.write('Celem naszego projektu było zbudowanie sieci neuronowej, która potrafi wykrywać ludzkie twarze na obrazach, a także w czasie rzeczywistym, np. z kamery w laptopie. Zbudowałyśmy także model rozpoznający (klasyfikujący) konkretne twarze, który wykorzystuje wiedzę na temat wykrywania dowolnych twarzy i jest rozszerzeniem zagadnienia detekcji twarzy.')
 
 
 with tabs[1]:
