807fb69c4d650419d0c7e4e8d8dab6c0e62a61ab
julia <168024513+juliraven@users.noreply.github.com>
2025-06-19 21:57:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,8 +81,6 @@ with cols[1]:
     st.markdown("<div id='outer_marker'></div>", unsafe_allow_html=True)
     with styled_container:
         st.markdown("<div id='gradient_container_marker'></div>", unsafe_allow_html=True)
-        st.markdown("<h2 style='text-align: center;'>Opis projektu</h2>", unsafe_allow_html=True)
-        st.markdown('')
         st.write('Projekt polegał na zbudowaniu sieci neuronowej, która potrafi wykrywać ludzkie twarze na obrazach, a także w czasie rzeczywistym, np. z kamery w laptopie. Zbudowałyśmy także model rozpoznający (klasyfikujący) konkretne twarze, który wykorzystuje wiedzę na temat wykrywania dowolnych twarzy i jest rozszerzeniem zagadnienia detekcji twarzy. Na kolejnych zakładkach znajdują się kody źródłowe napisane w Pythonie w ramach projektu, a także możliwości przetestowania modeli.')
 
 tabs = st.tabs(["Wykrywanie twarzy", "Klasyfikacja znanych twarzy", "Testuj na zdjęciu", "Testuj na żywo"])
