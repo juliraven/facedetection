19afd0af3a2409b6119d83170b42396774dc8680
julia <168024513+juliraven@users.noreply.github.com>
2025-06-19 14:27:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,108 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Wizualizacja danych - streamlit", layout="wide")
+
+page_bg_img_sidebar = """
+<style>
+/* Ustawienie szerokości sidebaru */
+section[data-testid="stSidebar"] {
+    width: 340px !important;
+    min-width: 340px !important;
+    max-width: 340px !important;
+    display: flex;
+    align-items: center;       /* Wyśrodkowanie w pionie */
+    justify-content: center;   /* Wyśrodkowanie w poziomie */
+    flex-direction: column;
+    height: 100vh;             /* Wysokość całego widoku */
+    padding-top: 10px;
+}
+
+/* Styl samego wnętrza sidebaru */
+[data-testid="stSidebar"] {
+    background: linear-gradient(
+        135deg,
+        rgba(32, 33, 37, 0.6),
+        rgba(45, 3, 94, 0.5),
+        rgba(180, 68, 251, 0.4)
+    );
+    border: 1px solid rgba(180, 68, 251, 0.3);
+    border-radius: 0px;
+    padding: 24px;
+    width: 100%;
+    box-shadow:
+        0 0 10px rgba(180, 68, 251, 0.25),
+        0 4px 16px rgba(0, 0, 0, 0.25);
+    backdrop-filter: blur(12px) brightness(1.05);
+    background-blend-mode: overlay;
+    transition: none;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  /* Wyśrodkowanie zawartości */
+}
+
+/* Główna treść */
+section.main > div {
+    padding-left: 220px !important;
+}
+
+/* Nagłówek przezroczysty */
+header[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+}
+
+/* Tło strony */
+body {
+    background-color: #202125;
+}
+</style>
+"""
+
+st.markdown(page_bg_img_sidebar, unsafe_allow_html=True)
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import altair as alt
+import base64
+import json
+import time
+from pyvis.network import Network
+import tempfile
+import re
+from streamlit_echarts import st_echarts
+
+st.markdown(
+    """
+    <style>
+    .stTabs [data-baseweb="tab-list"] {
+        justify-content: center;
+        margin-top: -70px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+tab1, tab2, tab3, tab4 = st.tabs(["Ludność", "Długość życia i śmiertelność", "Marvel", "Schemat kodu"])
+
+with tab1:
+
+    st.markdown("<h1 style='text-align: center;'>📊 Ludność świata na przestrzeni lat</h1>", unsafe_allow_html=True)
+    st.markdown(' ')
+
+
+
+
+
+
+
+
+
+'''
 import streamlit as st
 import torch
 from torchvision.models.detection import fasterrcnn_resnet50_fpn, FastRCNNPredictor
@@ -71,3 +176,4 @@ elif mode == "📹 Kamera (real-time)":
 
     webrtc_streamer(key="face-detection", video_processor_factory=FaceDetector)
 
+'''
