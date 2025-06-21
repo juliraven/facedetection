953598439d27f714570f67ddf66d3eafb388f6bc
julia <168024513+juliraven@users.noreply.github.com>
2025-06-21 04:21:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1427,10 +1427,9 @@ with tabs[2]:
         n1, n2, n3 = st.columns([1,5,1])
         with n2:
             cols = st.columns(len(example_images))  
-
             for col, (label, path) in zip(cols, example_images.items()):
                 with col:
-                    if st.button(label, key=label):  
+                    if st.button(label, key=f"btn_{label}"):  
                         selected_example = path
                     img = Image.open(path).convert("RGB")
                     st.image(img, use_container_width=True)
